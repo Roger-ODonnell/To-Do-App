43407ae0be3549e0d01bc7ce1471713ff33cd31d
Roger O'Donnell <71945647+Roger-ODonnell@users.noreply.github.com>
2025-02-23 23:26:02 +0000

    Added plan for next work day on what features to add to todo

--- site/JS/main.js
+++ site/JS/main.js
@@ -3,3 +3,5 @@ document.getElementById("button").addEventListener("click", () => {
     document.getElementById("button").style.textDecoration = "line-through";
   });
 });
+
+// Create page to display form to submit items for list, add to array, display items once done is clicked, add reset button
